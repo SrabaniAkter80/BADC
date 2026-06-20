26a3c7095800847db91b147f4031cfcacb07257f
SrabaniAkter80 <2310593@iub.edu.bd>
2026-06-20 20:46:36 +0600

    Added LoginController

---
+++ src/main/LoginController.java
@@ -0,0 +1,21 @@
+package org.example.loginscene;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginController
+{
+    @javafx.fxml.FXML
+    private PasswordField passwordPasswordField;
+    @javafx.fxml.FXML
+    private TextField UserNameTextField;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void OALogInButton(ActionEvent actionEvent) {
+    }
+}
