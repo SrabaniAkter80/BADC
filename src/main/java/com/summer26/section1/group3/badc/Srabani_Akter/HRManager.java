c7c9ca2e15ed2e0f540edc7bd8e86cce3e8712af
SrabaniAkter80 <2310593@iub.edu.bd>
2026-07-20 17:17:17 +0600

    Srabani_Akter_HR_Manager_Fxml

---
+++ src/main/java/com/summer26/section1/group3/badc/Srabani_Akter/HRManager.java
@@ -0,0 +1,4 @@
+package com.summer26.section1.group3.badc.Srabani_Akter;
+
+public class HRManager {
+}
--- src/main/java/com/summer26/section1/group3/badc/srabani/HRManager.java
+++
@@ -1,4 +0,0 @@
-package com.summer26.section1.group3.badc.srabani;
-
-public class HRManager {
-}
