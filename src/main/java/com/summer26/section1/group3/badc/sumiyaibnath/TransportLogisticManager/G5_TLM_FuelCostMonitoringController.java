e6e8eccd412c3b7e4f9847f2af73f3e454ee62fc
Sumiya Ibnath <2520977@iub.edu.bd>
2026-07-23 19:46:03 +0600

    TLM_G5,6

---
+++ src/main/java/com/summer26/section1/group3/badc/sumiyaibnath/TransportLogisticManager/G5_TLM_FuelCostMonitoringController.java
@@ -0,0 +1,41 @@
+package com.summer26.section1.group3.badc.sumiyaibnath.TransportLogisticManager;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class G5_TLM_FuelCostMonitoringController
+{
+    @javafx.fxml.FXML
+    private TableColumn colfuel;
+    @javafx.fxml.FXML
+    private TextField transportid;
+    @javafx.fxml.FXML
+    private TableView tableview;
+    @javafx.fxml.FXML
+    private TableColumn coltransportid;
+    @javafx.fxml.FXML
+    private TableColumn coldistance;
+    @javafx.fxml.FXML
+    private TableColumn colcost;
+    @javafx.fxml.FXML
+    private Label downloadlabel;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void download(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void search(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void back(ActionEvent actionEvent) {
+    }
+}
---
+++ src/main/java/com/summer26/section1/group3/badc/sumiyaibnath/TransportLogisticManager/G6_TLM_IssueManagementController.java
@@ -0,0 +1,37 @@
+package com.summer26.section1.group3.badc.sumiyaibnath.TransportLogisticManager;
+
+public class G6_TLM_IssueManagementController
+{
+    @javafx.fxml.FXML
+    private ComboBox issuetype;
+    @javafx.fxml.FXML
+    private TableColumn coldeliverystatus;
+    @javafx.fxml.FXML
+    private TextField transportid;
+    @javafx.fxml.FXML
+    private ComboBox deliverystatus;
+    @javafx.fxml.FXML
+    private TableColumn colissuetype;
+    @javafx.fxml.FXML
+    private TableView tableview;
+    @javafx.fxml.FXML
+    private Label label;
+    @javafx.fxml.FXML
+    private TableColumn coltransportid;
+    @javafx.fxml.FXML
+    private TableColumn colremarks;
+    @javafx.fxml.FXML
+    private TextField remarks;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void back(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void update(ActionEvent actionEvent) {
+    }
+}
