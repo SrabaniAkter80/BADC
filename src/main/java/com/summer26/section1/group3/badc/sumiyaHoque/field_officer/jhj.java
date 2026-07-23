3dd7ff75c0d3c86ec3ba570aa9f7738d12de6ba7
Samia Alam <samia.alam3047@gmail.com>
2026-07-23 12:12:18 +0600

    supplierGOAL1

---
+++ src/main/java/com/summer26/section1/group3/badc/sumiyaHoque/field_officer/jhj.java
@@ -0,0 +1,4 @@
+package com.summer26.section1.group3.badc.sumiyaHoque.field_officer;
+
+public class jhj {
+}
---
+++ src/main/java/com/summer26/section1/group3/badc/sumiyaHoque/supplier/GO_SP_DashboardController.java
@@ -0,0 +1,46 @@
+package com.summer26.section1.group3.badc.sumiyaHoque.supplier;
+
+import javafx.event.ActionEvent;
+
+public class GO_SP_DashboardController
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void PaymentNotifications(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void UploadDocuments(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void SupplyProposal(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void DeliverySchedule(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void SupplyStatus(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void LogOut(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void Profile(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void Contracts(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void SupplyReports(ActionEvent actionEvent) {
+    }
+}
