988fd42f9cc975325003107d9703b7d4614ebf37
Sumiya Ibnath <2520977@iub.edu.bd>
2026-07-26 21:46:25 +0600

    PO_G8, signup

---
+++ src/main/java/com/summer26/section1/group3/badc/common/SignupController.java
@@ -0,0 +1,32 @@
+package com.summer26.section1.group3.badc.common;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class SignupController
+{
+    @javafx.fxml.FXML
+    private TextField password;
+    @javafx.fxml.FXML
+    private ComboBox role;
+    @javafx.fxml.FXML
+    private Label label;
+    @javafx.fxml.FXML
+    private TextField confirmpassword;
+    @javafx.fxml.FXML
+    private TextField username;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void loginlink(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void signup(ActionEvent actionEvent) {
+    }
+}
