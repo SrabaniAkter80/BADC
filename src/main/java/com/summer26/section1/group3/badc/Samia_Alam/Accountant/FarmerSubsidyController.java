eaddafef15591161ac9e21f7f94267f3fe245cb4
samiaalam2004 <samia.alam3047@gmail.com>
2026-08-04 20:02:32 +0600

    Again add Accountant module

---
+++ src/main/java/com/summer26/section1/group3/badc/Samia_Alam/Accountant/FarmerSubsidyController.java
@@ -0,0 +1,32 @@
+package com.summer26.section1.group3.badc.Samia_Alam.Accountant;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class FarmerSubsidyController
+{
+    @javafx.fxml.FXML
+    private TableColumn nameTableCol;
+    @javafx.fxml.FXML
+    private TableView farmerSubsidyTableView;
+    @javafx.fxml.FXML
+    private TableColumn farmerIdTableCol;
+    @javafx.fxml.FXML
+    private TableColumn landAreaTableCol;
+    @javafx.fxml.FXML
+    private TextField subsidyAmountTF;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void disburseButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void backButton(ActionEvent actionEvent) {
+    }
+}
---
+++ src/main/java/com/summer26/section1/group3/badc/Samia_Alam/Accountant/OfficeExpenseController.java
@@ -0,0 +1,34 @@
+package com.summer26.section1.group3.badc.Samia_Alam.Accountant;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
+public class OfficeExpenseController
+{
+    @javafx.fxml.FXML
+    private DatePicker dateDatePicker;
+    @javafx.fxml.FXML
+    private TextField amountTextField;
+    @javafx.fxml.FXML
+    private ComboBox expenseTypeComboBox;
+    @javafx.fxml.FXML
+    private TextField voucharTextField;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void clearButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void RecordButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void backButton(ActionEvent actionEvent) {
+    }
+}
