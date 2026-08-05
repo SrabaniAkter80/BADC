d091de171349ce4c4eecbbbf80ace0ab1a0cddcb
samiaalam2004 <samia.alam3047@gmail.com>
2026-08-05 23:10:32 +0600

    Completed Admin module UI, controllers, models, and scene navigation

--- src/main/java/com/summer26/section1/group3/badc/Samia_Alam/Admin/ComplaintManagementController.java
+++ src/main/java/com/summer26/section1/group3/badc/Samia_Alam/Admin/ComplaintManagementController.java
@@ -1,35 +1,60 @@
 package com.summer26.section1.group3.badc.Samia_Alam.Admin;
 
+import com.summer26.section1.group3.badc.common.SceneSwitcher;
 import javafx.event.ActionEvent;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.*;
+
+import java.io.IOException;
 
 public class ComplaintManagementController
 {
     @javafx.fxml.FXML
-    private TableColumn farmerDealerTableCol;
+    private TableColumn <ComplaintManagement, String> farmerDealerTableCol;
     @javafx.fxml.FXML
-    private ComboBox fieldOfficerCB;
+    private ComboBox <String> fieldOfficerCB;
     @javafx.fxml.FXML
-    private TableColumn complaintIdTableCol;
+    private TableColumn <ComplaintManagement, String> complaintIdTableCol;
     @javafx.fxml.FXML
     private TextArea descriptionTA;
     @javafx.fxml.FXML
-    private TableColumn statusTableCol;
+    private TableColumn <ComplaintManagement, String> statusTableCol;
     @javafx.fxml.FXML
-    private TableView complaintTableView;
+    private TableView <ComplaintManagement> complaintTableView;
 
     @javafx.fxml.FXML
     public void initialize() {
+        fieldOfficerCB.getItems().addAll(
+                "Officer 1",
+                "Officer 2",
+                "Officer 3",
+                "Officer 4"
+        );
     }
 
     @javafx.fxml.FXML
     public void AssingOfficerButton(ActionEvent actionEvent) {
+        if (fieldOfficerCB.getValue() == null) {
+
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText(null);
+            alert.setContentText("Please select a Field Officer.");
+            alert.showAndWait();
+            return;
+        }
+
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Success");
+        alert.setHeaderText(null);
+        alert.setContentText("Field Officer assigned successfully.");
+        alert.showAndWait();
+
+        fieldOfficerCB.getSelectionModel().clearSelection();
+        descriptionTA.clear();
     }
 
     @javafx.fxml.FXML
-    public void backButton(ActionEvent actionEvent) {
+    public void backButton(ActionEvent actionEvent) throws IOException {
+        SceneSwitcher.switchTo("/com/summer26/section1/group3/badc/Samia_Alam/Admin/AdminDashboard.fxml");
     }
 }
