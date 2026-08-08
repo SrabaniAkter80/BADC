6e31d54e860400ff7ab44e57a4d6bd04534fd3ea
SrabaniAkter80 <2310593@iub.edu.bd>
2026-08-08 09:58:31 +0600

    Farmer notification dymmy object created and run

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.summer26.section1.group3.badc {
     requires javafx.fxml;
     requires com.github.librepdf.openpdf;
 
+
     exports com.summer26.section1.group3.badc.common;
     opens com.summer26.section1.group3.badc.common to javafx.fxml;
     opens com.summer26.section1.group3.badc.sumiyaibnath.ProcurementOfficer to javafx.fxml;
